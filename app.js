184f0953943462ef2f9dc01a8c6ef7adf1fb8819
yoel <alan.yoel.caba@gmail.com>
2025-01-20 22:25:38 -0300

    v1.1.0

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arrayAmigos = [];
+
+function agregarAmigo(){
+    let amigo = document.getElementById("amigo");
+    validarCampoVacio(amigo);
+    limpiarCampo(amigo);
+}
+
+function limpiarCampo(campoAmigo){
+    campo.textContent("");
+}
+
+function validarCampoVacio(amigo){
+    if (amigo != empy){
+        arrayAmigos.push(amigo);
+    } else {
+        alert("Por favor, inserte un nombre.")
+    }
+}
+
+function sortearAmigo(){
+
+}
+
